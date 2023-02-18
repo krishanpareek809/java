7ef731d08a9395a6ca734d539f7116fd97bc8370
Krishan Pareek <krishanpareek809@gmail.com>
2023-02-19 01:17:16 +0530

    Pattern Questions

--- Patterns.java
+++ Patterns.java
@@ -2,6 +2,8 @@ public class Patterns {
     public static void main(String[] args) {
         int i;
         int j;
+        int n = 5;
+        int number = 1;
 
         // Solid Rectangle
         // for(i=1; i<=4; i++){
@@ -38,9 +40,64 @@ public class Patterns {
 
 
         // Inverted Half Pyramid
-        for(i=4;i>=1;i--){
-            for(j=4;j>=i;j--){
-                System.out.print("*");
+        // for(i=4;i>=1;i--){
+        //     for(j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        
+        // // Inverted Half Pyramid(rotated by 180 degree)         //       *
+        // // outer loop                                           //     * *
+        // for(i=1;i<=n;i++){                                      //   * * *
+        //     // inner loop -> to print space                     // * * * *
+        //     for(j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     // inner loop -> to print star
+        //     for(j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for(i=1;i<=n;i++){
+        //     for(j=1;j<=i;j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for(i=1;i<=n;i++){                      // LEARN THE DRY FOR THIS CODE
+        //     for(j=1;j<=n-i+1;j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Floyd's Triangle
+        
+        // for(i=1;i<=n;i++){
+        //     for(j=1;j<=i;j++){
+        //         System.out.print(number+" ");
+        //         number++;
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // 0 1 triangle
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1" + " ");
+                } else {
+                    System.out.print("0" + " ");
+                }
             }
             System.out.println();
         }
