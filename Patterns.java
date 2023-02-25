b58f3662673ed1d1f00b2f20bbec3c15fe52b98d
Krishan Pareek <krishanpareek809@gmail.com>
2023-02-25 22:59:21 +0530

    Butterfly Pattern Added

--- Patterns.java
+++ Patterns.java
@@ -1,13 +1,11 @@
 public class Patterns {
     public static void main(String[] args) {
-        int i;
-        int j;
         int n = 5;
         int number = 1;
 
         // Solid Rectangle
-        // for(i=1; i<=4; i++){
-        //     for(j=1; j<=5; j++){
+        // for(int i=1; i<=4; i++){
+        //     for(int j=1; j<=5; j++){
         //         System.out.print("*");
         //     }
         //     System.out.println();
@@ -15,12 +13,12 @@ public class Patterns {
 
 
         // // Hollow Rectangle                      // *  *  *  *  *
-        //     // outer loop                        // *           *
-        // for(i=1; i<=4; i++){                     // *           *
+        // // outer loop                            // *           *
+        // for(int i=1; i<=4; i++){                 // *           *
         //     // inner loop                        // *  *  *  *  *
-        //     for(j=1; j<=5; j++){
+        //     for(int j=1; j<=5; j++){
         //         // cell -> (i,j)
-        //         if(i==1 || j==1 || i==4 || j==5){
+        //         if(int i==1 || j==1 || i==4 || j==5){
         //             System.out.print("*");
         //         } else{
         //             System.out.print(" ");
@@ -31,8 +29,8 @@ public class Patterns {
 
 
         // Half Pyramid
-        // for(i=1;i<=4;i++){
-        //     for(j=1;j<=i;j++){
+        // for(int i=1;i<=4;i++){
+        //     for(int j=1;j<=i;j++){
         //         System.out.print("*");
         //     }
         //     System.out.println();
@@ -40,8 +38,8 @@ public class Patterns {
 
 
         // Inverted Half Pyramid
-        // for(i=4;i>=1;i--){
-        //     for(j=1;j<=i;j++){
+        // for(int i=4;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
         //         System.out.print("*");
         //     }
         //     System.out.println();
@@ -50,29 +48,29 @@ public class Patterns {
         
         // // Inverted Half Pyramid(rotated by 180 degree)         //       *
         // // outer loop                                           //     * *
-        // for(i=1;i<=n;i++){                                      //   * * *
+        // for(int i=1;i<=n;i++){                                  //   * * *
         //     // inner loop -> to print space                     // * * * *
-        //     for(j=1;j<=n-i;j++){
+        //     for(int j=1;j<=n-i;j++){
         //         System.out.print(" ");
         //     }
         //     // inner loop -> to print star
-        //     for(j=1;j<=i;j++){
+        //     for(int j=1;j<=i;j++){
         //         System.out.print("*");
         //     }
         //     System.out.println();
         // }
 
 
-        // for(i=1;i<=n;i++){
-        //     for(j=1;j<=i;j++){
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
         //         System.out.print(j+" ");
         //     }
         //     System.out.println();
         // }
 
 
-        // for(i=1;i<=n;i++){                      // LEARN THE DRY FOR THIS CODE
-        //     for(j=1;j<=n-i+1;j++){
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i+1;j++){
         //         System.out.print(j+" ");
         //     }
         //     System.out.println();
@@ -80,9 +78,8 @@ public class Patterns {
 
 
         // Floyd's Triangle
-        
-        // for(i=1;i<=n;i++){
-        //     for(j=1;j<=i;j++){
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
         //         System.out.print(number+" ");
         //         number++;
         //     }
@@ -91,15 +88,15 @@ public class Patterns {
 
 
         // 0 1 triangle
-        for(i=1;i<=n;i++){
-            for(j=1;j<=i;j++){
-                if((i+j)%2==0){
-                    System.out.print("1" + " ");
-                } else {
-                    System.out.print("0" + " ");
-                }
-            }
-            System.out.println();
-        }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         if((i+j)%2==0){
+        //             System.out.print("1" + " ");
+        //         } else {
+        //             System.out.print("0" + " ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
     }
 }
