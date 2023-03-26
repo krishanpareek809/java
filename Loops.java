e00bbb5cbb1bfc1ca443b42180ee1c6c4242db2a
Krishan Pareek <krishanpareek809@gmail.com>
2023-03-26 19:12:38 +0530

    loops practice

--- Loops.java
+++ Loops.java
@@ -36,12 +36,50 @@ public class Loops {
         // }
 
 
-        // factorial of a number
-        int fact = 1;
-        for(int i=1;i<=n;i++){
-            fact = fact*i;
-        }
-        System.out.println(fact);
+        // factorial of a number using for loop
+        // int fact = 1;
+        // for(int i=1;i<=n;i++){
+        //     fact = fact*i;
+        // }
+        // System.out.println(fact);
 
+
+        // factorial of a number using while loop
+        // int fact = 1;
+        // int i = 1;
+        // while(i<=n){
+        //     fact = fact*i;
+        //     i++;
+        // }
+        // System.out.println(fact);
+
+
+        // int i = 1, j = 1, n = 5;
+        // while(i<=n){
+        //     while(j<=i){
+        //         System.out.print("*");
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        //     j = 1;       // WHY???????
+        // }
+
+
+        // int i = 1, n=8, sum=0;
+        // for(i=1;i<=10;i++){
+        //     // sum = sum + (n*i);
+        //     sum += n*i;
+        // }
+        // System.out.printf("Sum of numbers occuring in the table of 8 is: " + sum);
+
+
+        // int i = 1;
+        // int sum = 0;
+        // while(i<=n){
+        //     sum = sum + (2*i);
+        //     i++;
+        // }
+        // System.out.println("Sum of first n even numbers is: " + sum);
     }
 }
