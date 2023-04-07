b71dd6507bda6ad1ac3dddab5c8a917424780a3b
Krishan Pareek <krishanpareek809@gmail.com>
2023-04-07 21:19:06 +0530

    arrays in java

---
+++ Arrays.java
@@ -0,0 +1,45 @@
+class Arrays {
+    public static void main(String[] args) {
+        // int[] marks = new int[3];       // 1-D Array
+        // marks[0] = 1;
+        // marks[1] = 10;
+        // marks[2] = 45;
+        // System.out.println(marks[2]);
+
+        // Length of an array
+        // System.out.println(marks.length);
+
+        // Displaying an array using for loop
+        // for(int i=0;i<marks.length;i++){
+        // System.out.println(marks[i]);
+        // }
+
+        // for (int i=marks.length-1; i>=0; i--){
+        // System.out.println(marks[i]);
+        // }
+
+        // Displaying an array using for-each loop
+        // for(int element:marks){
+        // System.out.println(element);
+        // }
+
+        // Multidimensional Arrays
+        // int [][] flats = new int[2][3];      // 2-D Array
+        // flats[0][0] = 101;
+        // flats[0][1] = 102;
+        // flats[0][2] = 103;
+        // flats[1][0] = 201;
+        // flats[1][1] = 202;
+        // flats[1][2] = 203;
+
+        // for (int i=0; i<flats.length; i++) {
+        //     for(int j=0; j<flats[i].length; j++){
+        //         System.out.print(flats[i][j]);
+        //         System.out.print(" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        
+    }
+}
