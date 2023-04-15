b915486fdac2f1d330931c33b32586b4650885a5
Krishan Pareek <krishanpareek809@gmail.com>
2023-04-15 23:52:13 +0530

    arrays practice

--- Arrays.java
+++ Arrays.java
@@ -95,5 +95,66 @@ class Arrays {
         //     }
         //     System.out.println();
         // }
+
+
+        // Program to reverse an array
+        // int[] arr = {1,2,56,7,45,67};
+        // int l = arr.length;
+        // int n = Math.floorDiv(l, 2);
+        // int temp;
+
+        // for(int i=0; i<n; i++){
+        //     // swap a[i] and a[l-i-1]
+        //     //  a   b  temp
+        //     // [4] [3]  ||
+        //     temp = arr[i];
+        //     arr[i] = arr[l-i-1];
+        //     arr[l-i-1] = temp;
+        // }
+        // for(int element: arr){
+        //     System.out.print(element + " ");
+        // }
+
+        
+        // TO FIND MAXIMUM ELEMENT IN AN ARRAY
+        // int[] arr = {1,45333,6778,465,3,2,45};
+        // int max = Integer.MIN_VALUE;
+
+        // for(int element: arr){
+        //     if(element>max){
+        //         max = element;
+        //     }
+        // }
+        // System.out.println(max);
+
+
+        // TO FIND MINIMUM ELEMENT IN AN ARRAY
+        // int[] arr = {45333,6778,465,3,2,45};
+        // int min = Integer.MAX_VALUE;
+
+        // for(int element: arr){
+        //     if(element<min){
+        //         min = element;
+        //     }
+        // }
+        // System.out.println(min);
+
+
+        // Program to find whether an array is sorted or not
+        // int[] arr = {1,2,3,45,465,6778,45333};  
+        int[] arr = {1,45333,6778,465,3,2,45};  
+        boolean isSorted = true;
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+
+        if(isSorted){
+            System.out.println("The array is sorted");
+        } else{
+            System.out.println("The array is not sorted");
+        }
     }
 }
