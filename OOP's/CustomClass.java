bc62de459ae35e8ac5bb6d4719286041e8856665
Krishan Pareek <krishanpareek809@gmail.com>
2023-07-09 21:33:15 +0530

    OOP's Practice

--- OOP's/CustomClass.java
+++ OOP's/CustomClass.java
@@ -26,6 +26,8 @@ public class CustomClass{
 }
 */
 
+
+/*
 class Employee{
     int salary;
     String name;
@@ -55,5 +57,62 @@ public class CustomClass{
         
         krishan.setName("krish");
         System.out.println(krishan.getName());
+    }
+}
+*/
+
+
+/*
+class Square{
+    int side;
+
+    public int side(){
+        return side;
+    }
+
+    public int area(){
+        return side*side;
+    }
+
+    public int perimeter(){
+        return 4*side;
+    }
+}
+
+public class CustomClass{
+    public static void main(String[] args) {
+        Square sq = new Square();
+
+        sq.side = 3;
+
+        System.out.println(sq.side());
+        System.out.println(sq.area());
+        System.out.println(sq.perimeter());
+    }
+}
+*/
+
+
+class TommyVecetti{
+    public void hit(){
+        System.out.println("Hitting the enemy");
+    }
+
+    public void run(){
+        System.out.println("Running for loot");
+    }
+
+    public void fire(){
+        System.out.println("Firing for safety");
+    }
+}
+
+public class CustomClass{
+    public static void main(String[] args) {
+        TommyVecetti Player1 = new TommyVecetti();
+
+        Player1.hit();
+        Player1.run();
+        Player1.fire();
     }
 }
