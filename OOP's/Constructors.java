cf6245bef35c820ea1b668fcd127f6314a6053ab
Krishan Pareek <krishanpareek809@gmail.com>
2023-08-07 00:01:33 +0530

    Copy Constructors in Java

--- OOP's/Constructors.java
+++ OOP's/Constructors.java
@@ -1,3 +1,4 @@
+/*
 class Employee{
 
     private int id;
@@ -43,5 +44,41 @@ public class Constructors{
 
         System.out.println(krishan.getName());
         System.out.println(krishan.getId());
+    }
+}
+*/
+
+
+// Copy Constructors
+
+import javax.swing.text.Style;
+
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+    Student(Student s2){
+        this.name = s2.name;
+        this.age = s2.age;
+    }
+    
+    Student(){
+
+    }
+}
+
+public class Constructors{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Krishan";
+        s1.age = 22;
+
+        Student s2 = new Student(s1);
+        s2.printInfo();
     }
 }
