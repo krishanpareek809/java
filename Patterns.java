9cec59dced73846f62fdec3d4b599b08d53c07a0
Krishan Pareek <krishanpareek809@gmail.com>
2023-08-18 00:42:05 +0530

    Solid Rhombus Pattern Question

--- Patterns.java
+++ Patterns.java
@@ -98,5 +98,20 @@ public class Patterns {
         //     }
         //     System.out.println();
         // }
+
+
+        // Solid Rhombus
+        for(int i=1; i<=n; i++){
+            // spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j=1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();  
+        }
     }
 }
